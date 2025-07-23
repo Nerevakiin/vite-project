8f7404f9ff6ac15e672d52c44bd4040f599e3b03
Nerevakiin <djmatisk@yahoo.gr>
2025-07-24 01:57:48 +0300

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import { createElement } from "react"
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+root.render(
+    <main>
+        <img src="react-logo.png" />
+        <h1>This is another element</h1>
+    </main>
+)
