7c50e340f6902e075e65c669a9b7cb600eb71fea
Nerevakiin <djmatisk@yahoo.gr>
2025-08-17 01:38:00 +0300

    This diff contains a change in line endings from 'LF' to 'CRLF'.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,5 @@
-import { createElement } from "react"
 import { createRoot } from "react-dom/client"
+import App from "./App"
+
 const root = createRoot(document.getElementById("root"))
-root.render(
-    <main>
-        <img src="react-logo.png" />
-        <h1>This is another element</h1>
-    </main>
-)
+root.render(<App />) 
